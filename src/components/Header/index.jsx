256e35cdeb9155c37607a66a6717116b0a88912e
Emanuel Jabon <138291892+vv4nity@users.noreply.github.com>
2025-08-18 01:01:09 +0800

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,77 +9,69 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Rounded from '../../common/RoundedButton';
 import Magnetic from '../../common/Magnetic';
 
-export default function Index() {
+export default function index() {
     const header = useRef(null);
     const [isActive, setIsActive] = useState(false);
     const pathname = usePathname();
     const button = useRef(null);
 
-    useEffect(() => {
-        if (isActive) setIsActive(false);
-    }, [pathname]);
+    useEffect( () => {
+      if(isActive) setIsActive(false)
+    }, [pathname])
 
-    useLayoutEffect(() => {
-        gsap.registerPlugin(ScrollTrigger);
+    useLayoutEffect( () => {
+        gsap.registerPlugin(ScrollTrigger)
         gsap.to(button.current, {
             scrollTrigger: {
                 trigger: document.documentElement,
                 start: 0,
                 end: window.innerHeight,
-                onLeave: () => {
-                    gsap.to(button.current, { scale: 1, duration: 0.25, ease: "power1.out" })
-                },
-                onEnterBack: () => {
-                    gsap.to(button.current, { scale: 0, duration: 0.25, ease: "power1.out" }, setIsActive(false))
-                }
+                onLeave: () => {gsap.to(button.current, {scale: 1, duration: 0.25, ease: "power1.out"})},
+                onEnterBack: () => {gsap.to(button.current, {scale: 0, duration: 0.25, ease: "power1.out"},setIsActive(false))}
             }
         })
-    }, []);
+    }, [])
 
     return (
         <>
-            <div ref={header} className={styles.header}>
-                <div className={styles.logo}>
-                    <p className={styles.copyright}>©</p>
-                    <div className={styles.name}>
-                        <div className={styles.codeByWrapper}>
-                            <p className={styles.codeBy}>Code by</p>
-                        </div>
-                        <p className={styles.dennis}>Emanuel</p>
-                        <p className={styles.snellenberg}>Jabon</p>
+        <div ref={header} className={styles.header}>
+            <div className={styles.logo}>
+                <p className={styles.copyright}>©</p>
+                <div className={styles.name}>
+                    <p className={styles.codeBy}>Code by</p>
+                    <p className={styles.dennis}>Emanuel</p>
+                    <p className={styles.snellenberg}>Jabon</p>
+                </div>
+            </div>
+            <div className={styles.nav}>
+                <Magnetic>
+                    <div className={styles.el}>
+                        <a>Work</a>
+                        <div className={styles.indicator}></div>
                     </div>
-                </div>
-                <div className={styles.nav}>
-                    <Magnetic>
-                        <div className={styles.el}>
-                            <a>Work</a>
-                            <div className={styles.indicator}></div>
-                        </div>
-                    </Magnetic>
-                    <Magnetic>
-                        <div className={styles.el}>
-                            <a>About</a>
-                            <div className={styles.indicator}></div>
-                        </div>
-                    </Magnetic>
-                    <Magnetic>
-                        <div className={styles.el}>
-                            <a>Contact</a>
-                            <div className={styles.indicator}></div>
-                        </div>
-                    </Magnetic>
-                </div>
+                </Magnetic>
+                <Magnetic>
+                    <div className={styles.el}>
+                        <a>About</a>
+                        <div className={styles.indicator}></div>
+                    </div>
+                </Magnetic>
+                <Magnetic>
+                    <div className={styles.el}>
+                        <a>Contact</a>
+                        <div className={styles.indicator}></div>
+                    </div>
+                </Magnetic>
             </div>
-
-            <div ref={button} className={styles.headerButtonContainer}>
-                <Rounded onClick={() => { setIsActive(!isActive) }} className={`${styles.button}`}>
-                    <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
-                </Rounded>
-            </div>
-
-            <AnimatePresence mode="wait">
-                {isActive && <Nav />}
-            </AnimatePresence>
+        </div>
+        <div ref={button} className={styles.headerButtonContainer}>
+            <Rounded onClick={() => {setIsActive(!isActive)}} className={`${styles.button}`}>
+                <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
+            </Rounded>
+        </div>
+        <AnimatePresence mode="wait">
+            {isActive && <Nav />}
+        </AnimatePresence>
         </>
     )
 }
